5e998b7b79522aa8e7bdf93b874705124a485e97
Guilhermwn <limaoplaykglp@gmail.com>
2025-04-05 22:40:17 -0300

    organization and docstrings - organized the modules in folders - added docstrings to statistic descriptive functions

--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,16 @@
 import statistics as s
 import math as mt
 
-from src.maphrs import m_statistics as m
+from maphrs.stats.descriptive import standard_deviation
 
 x = [10,34,23,54,9]
+y = [1,5,6,10,13]
 
-assert m.mean(x) == s.mean(x)
-assert m.sqrt(2) == mt.sqrt(2)
-assert m.standard_deviation(x) == s.stdev(x)
+print(standard_deviation(x))
+
+# assert m_s.mean(x) == s.mean(x)
+# assert m_s.sqrt(2) == mt.sqrt(2)
+# assert m_s.standard_deviation(x) == s.stdev(x)
+# assert m_s.relative_error(9.76, 9.78) == ((9.76 - 9.78) / 9.78) * 100
+
+save_opt = {"save":True, "filename": "plot", "format":"svg", "bytes": True}
