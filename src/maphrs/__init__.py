3b43771e38cc3e8275ea663cfb3471e150d508b5
Guilhermwn <limaoplaykglp@gmail.com>
2025-04-14 19:38:33 -0300

    fix: update dependencies and imports

--- src/maphrs/__init__.py
+++ src/maphrs/__init__.py
@@ -1,8 +1,15 @@
-from maphrs.objects.eletronics import Resistor, COMMERCIAL_RESISTORS
-from maphrs.statistics.descriptive import relative_error
-
-__all__ = [
-    'Resistor',
-    'COMMERCIAL_RESISTORS',
-    'relative_error'
-]
+from maphrs.objects.eletronics import (
+    Resistor, ResistorUnit, CommercialResistors, COMMERCIAL_RESISTORS
+)
+from maphrs.statistics.descriptive import (
+    root, sqrt, mean, standard_deviation, relative_error
+) 
+from maphrs.eletronics.basic import (
+    find_resistor_association
+)
+from maphrs.statistics.uncertainty import (
+    uncertainty_a, uncertainty_c
+)
+from maphrs.visualization.statistical import (
+    line_plot
+)
